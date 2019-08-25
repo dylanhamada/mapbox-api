6fec1849defa855bdfd0d1b5c22c08256918b89b
Dylan Hamada <dylan.hamada@gmail.com>
2019-08-25 15:45:15 -0400

    Fix geoApiCall and dropDownMenu to accurately display search calls to Geocoding API

--- scripts/index.js
+++ scripts/index.js
@@ -16,15 +16,18 @@ function keyStrokes(event) {
 the value in the search bar, and stores the response array in a global 
 locationArr variable */
 function geoApiCall(searchVal) {
+  // Use regedit to format white space in search result
+  const formattedSearch = searchVal.replace(/\s/g, `%20`);
   const accessKey =
     "pk.eyJ1IjoiZHlsYW5oYW1hZGEiLCJhIjoiY2p6ZzN4Nm4yMGZlMTNibG5rZHgxaWlweiJ9.sq7TBW2_ZeVF588ALeME-A";
-  const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchVal}.json?access_token=${accessKey}`;
+  const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${formattedSearch}.json?proximity=-79.38463116005312,43.652681912152616&bbox=-79.45205426676789,43.62528813725265,-79.31853054929633,43.697591723318226&access_token=${accessKey}`;
 
-  if (searchVal) {
+  if (formattedSearch) {
     let request = new XMLHttpRequest();
 
     request.open("GET", apiUrl, true);
 
+    // Iterate through API response and store locations in array
     request.onload = function() {
       let locationData = JSON.parse(this.response);
 
@@ -37,6 +40,10 @@ function geoApiCall(searchVal) {
 
     request.send();
   }
+  // If search term is empty string, reset location array
+  else {
+    locationArr = [];
+  }
 
   return locationArr;
 }
@@ -47,10 +54,12 @@ function dropDownMenu(locationArr) {
   let locationName = locationArr.map(location => location.text);
   let resultsDivs = document.querySelectorAll(".searchResults");
 
+  // Remove all searchResult divs whenever function is called, ie "refresh"
   resultsDivs.forEach(div => {
     div.remove();
   });
 
+  // If location array isn't empty, create new divs in nav bar for individual search results
   if (locationArr.length > 0) {
     locationName.forEach(name => {
       let newDiv = document.createElement("div");
@@ -62,4 +71,4 @@ function dropDownMenu(locationArr) {
   }
 }
 
-searchBar.addEventListener("keydown", keyStrokes);
+searchBar.addEventListener("keyup", keyStrokes);
